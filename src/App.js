d5016636cc1853730dd0963d49472c7f4fbd6bba
António Alvarenga <aalvarenga@imaginarycloud.com>
2021-05-21 00:00:44 +0100

    add first components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 10,
+      date: new Date(2020, 7, 14)
+    },
+    {
+      id: 'e2',
+      title: 'TV',
+      amount: 799.99,
+      date: new Date(2021, 2, 112)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.65,
+      date: new Date(2021, 2, 28)
+    }
+  ] 
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
