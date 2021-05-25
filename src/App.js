b56ff6f3020afbe66a59f924834972f56ca11247
António Alvarenga <aalvarenga@imaginarycloud.com>
2021-05-25 20:22:12 +0100

    ExpensesList component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        title: "Toilet Paper",
+        amount: 10,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: "e2",
+        title: "TV",
+        amount: 799.99,
+        date: new Date(2021, 2, 112),
+    },
+    {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.65,
+        date: new Date(2021, 2, 28),
+    },
+];
+
 function App() {
-    const expenses = [
-        {
-            id: "e1",
-            title: "Toilet Paper",
-            amount: 10,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: "e2",
-            title: "TV",
-            amount: 799.99,
-            date: new Date(2021, 2, 112),
-        },
-        {
-            id: "e3",
-            title: "Car Insurance",
-            amount: 294.65,
-            date: new Date(2021, 2, 28),
-        },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
     const addExpenseHandler = (newExpense) => {
-        console.log(newExpense)
+        setExpenses(prevExpenses => [newExpense, ...prevExpenses])
     };
 
     return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,29 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
-    const [selectedYear, setSelectedYear] = useState('2021')
+    const [selectedYear, setSelectedYear] = useState("2021");
 
-    const selectedYearHandler = year => {
-        setSelectedYear(year)
-        console.log(selectedYear)
-    }
+    const filteredExpenses = props.expenses.filter(
+        (expense) => expense.date.getFullYear().toString() === selectedYear
+    );
+
+    const selectedYearHandler = (year) => {
+        setSelectedYear(year);
+    };
 
     return (
-            <Card className="expenses">
-                <ExpensesFilter onYearSelect={selectedYearHandler} selectedYear={selectedYear}/>
-                <ExpenseItem
-                    title={props.expenses[0].title}
-                    amount={props.expenses[0].amount}
-                    date={props.expenses[0].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[1].title}
-                    amount={props.expenses[1].amount}
-                    date={props.expenses[1].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[2].title}
-                    amount={props.expenses[2].amount}
-                    date={props.expenses[2].date}
-                />
-            </Card>
+        <Card className="expenses">
+            <ExpensesFilter
+                onYearSelect={selectedYearHandler}
+                selectedYear={selectedYear}
+            />
+            <ExpensesList expenses={filteredExpenses} />
+        </Card>
     );
 }
 
-export default Expenses
+export default Expenses;
