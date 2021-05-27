ab0d25ac6c41fce45a1f28731c43b5b9bdd20e4e
António Alvarenga <aalvarenga@imaginarycloud.com>
2021-05-28 00:38:36 +0100

    finish expenses tracker

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,41 @@
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm'
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+    const [showForm, setFormVisibility] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
-            id: Math.random().toString()
-        }
-        props.onAddExpense(expenseData)
-    }
+            id: Math.random().toString(),
+        };
+        props.onAddExpense(expenseData);
+        setFormVisibility(false);
+    };
+
+    const addNewExpenseHandler = () => {
+        setFormVisibility(true);
+    };
+
+    const cancelHandler = () => {
+        setFormVisibility(false);
+    };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {showForm ? (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={cancelHandler}
+                />
+            ) : (
+                <button type="button" onClick={addNewExpenseHandler}>
+                    Add New Expense
+                </button>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default NewExpense
+export default NewExpense;
